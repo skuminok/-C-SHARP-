a830ebe41c4ef71b5abbc58b5e6d0f841f887dd3
skuminok <154214571+skuminok@users.noreply.github.com>
2024-01-09 18:48:02 +0500

    Lesson_1:Task_1

---
+++ lesson_1/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Enter a number 1: ");
+int b1 = int.Parse( Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+
+if (b2 * b2 == b1)
+     Console.WriteLine("Yes");
+else
+     Console.WriteLine("No");
+ 
